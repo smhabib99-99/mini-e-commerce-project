5bca31222a66781ad8736175cdbafb085b7a849e
smhabib99-99 <habibaust34@gmail.com>
2025-08-11 21:56:17 +0600

    products data array of objects creation using associative array where we can get key, value using arrow

---
+++ products_data.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "classes/Product.php";
+
+$products = [
+
+    1 => new Product(1, "ipad", 600, "assets/image/ipad.jpg"),
+    2 => new Product(2, "book", 30, "assets/image/book.jpg"),
+    3 => new Product(3, "bike", 2000, "assets/image/bike.jpg"),
+    4 => new Product(4, "shoes", 100, "assets/image/shoes.jpg"),
+    5 => new Product(5, "shirt", 10, "assets/image/shirt.jpg"),
+    6 => new Product(6, "cap", 5, "assets/image/cap.jpg"),
+    7 => new Product(7, "watch", 150, "assets/image/watch.jpg"),
+    8 => new Product(8, "car", 20000, "assets/image/car.jpg"),
+
+];
